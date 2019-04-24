8c543c2dc1617cf1d3f5c0a6a1457635a3be7914
Ana Carolina Fidelis Gonçalves <acarolinafg@gmail.com>
2019-04-24 00:50:34 -0300

    Projeto inicial

---
+++ trabalho1/src/main/java/br/ufjf/dcc193/trabalho1/MainApplication.java
@@ -0,0 +1,14 @@
+package br.ufjf.dcc193.trabalho1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MainApplication.class, args);
+		System.out.println("Hello word!");
+	}
+
+}
